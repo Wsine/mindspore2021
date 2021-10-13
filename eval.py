ba7c2b4b20acb969465cd526a7a9e18004a641a0
Wsine <weizhy3@hotmail.com>
2021-10-13 12:14:10 +0800

    resume the preprocess

--- eval.py
+++ eval.py
@@ -21,22 +21,33 @@ def get_model_net_with_weights(ckpt_file='./yolov3.ckpt'):
 def run_eval(opt):
     net = get_model_net_with_weights()
 
-    dataset = create_yolo_dataset(
-        opt.mindrecord_file, batch_size=opt.batch_size,
-        device_num=1, rank=0, is_training=False
-    )
-    dataset_size = dataset.get_dataset_size()
-    print(f"Dataset Created with num of batches: {dataset_size}")
+    #  dataset = create_yolo_dataset(
+    #      opt.mindrecord_file, batch_size=opt.batch_size,
+    #      device_num=1, rank=0, is_training=False
+    #  )
+    #  dataset_size = dataset.get_dataset_size()
+    #  print(f"Dataset Created with num of batches: {dataset_size}")
+
+    meta = open('metadata.csv')
 
     results = []
     xai_results = []
     num_class = {0:'SCC', 1: 'AC', 2:'SCLC', 3: 'NSCLC'}
-    for data in dataset.create_dict_iterator(output_numpy=True):
-        image_file = data['file']  # type: ignore
-        image_id = image_file.tobytes().decode('ascii')
+    #  for data in dataset.create_dict_iterator(output_numpy=True):
+    #      image_file = data['file']  # type: ignore
+    #      image_id = image_file.tobytes().decode('ascii').rstrip('.bmp')
+    #      print(image_id)
+    #      image = data['image']  # type: ignore
+    #      image = data['image'][0].astype(np.uint8)  # type: ignore
+    #      #  image_shape = data['image_shape']  # type: ignore
+
+    for line in meta.readlines()[1:]:
+        image_id, width, height, _ = line.strip().split(',')
         print(image_id)
-        image = data['image']  # type: ignore
-        image_shape = data['image_shape']  # type: ignore
+        image_path = os.path.join('dataset', 'images', image_id + '.bmp')
+        image = np.asarray(Image.open(image_path), dtype=np.uint8).transpose((2, 0, 1))
+        image_shape = (int(height), int(width))
+
         if hasattr(participant_model, 'pre_process'):
             pre_processed_data = participant_model.pre_process(
                 image_id, image
@@ -67,6 +78,10 @@ def run_eval(opt):
 
         results.append(result)
         #  xai_results.append(xai_result)
+        if len(results) > 10:
+            break
+
+    meta.close()
 
 
 def draw_prediction(opt, image_id, img_np, pred, num_class):
@@ -76,32 +91,33 @@ def draw_prediction(opt, image_id, img_np, pred, num_class):
 
     boxes = pred[:, :4]
     box_scores = pred[:, 4:]
-    #  classes = np.max(box_scores)
-    #  boxes = pred[0].asnumpy()[batch_idx]
-    #  box_scores = pred[1].asnumpy()[batch_idx]
-    #  boxes, classes, scores = tobox(boxes, box_scores)
+    classes = np.argmax(box_scores, axis=1)
+    scores = np.max(box_scores, axis=1)
 
     fig = plt.figure()
     ax = fig.add_subplot(1, 1, 1)
 
-    image_path = os.path.join('dataset', 'images', image_id)
+    image_path = os.path.join('dataset', 'images', image_id + '.bmp')
     f = Image.open(image_path)
     img_np = np.asarray(f, dtype=np.float32)
     ax.imshow(img_np.astype(np.uint8))
+    h, w, _ = img_np.shape
 
     # draw prediction
     for box_index in range(boxes.shape[0]):
-        ymin=boxes[box_index][0]
-        xmin=boxes[box_index][1]
-        ymax=boxes[box_index][2]
-        xmax=boxes[box_index][3]
+        ymin = boxes[box_index][0] * w
+        xmin = boxes[box_index][1] * h
+        ymax = boxes[box_index][2] * w
+        xmax = boxes[box_index][3] * h
         ax.add_patch(plt.Rectangle(
-            (xmin,ymin), (xmax-xmin), (ymax-ymin), fill=False, edgecolor='red', linewidth=1
+            (xmin,ymin), (xmax-xmin), (ymax-ymin),
+            fill=False, edgecolor='red', linewidth=1
         ))
-        #  ax.text(xmin, ymin, s=str(num_class[classes[box_index]]) + str(scores[box_index]),
-        #          style='italic', c ='red', bbox={'alpha': 0.5})
-        ax.text(xmin, ymin, s=str(box_scores[box_index]),
-                style='italic', c ='red', bbox={'alpha': 0.5})
+        ax.text(
+            xmin, ymin,
+            s=f'{num_class[classes[box_index]]}: {scores[box_index]}',
+            style='italic', c ='red', bbox={'alpha': 0.5}
+        )
 
     save_file = os.path.join(opt.draw_dir, image_id.rstrip('.bmp') + '.png')
     print(save_file)
